de0d01a9f8ef7aaaf8ccabe77f95e690f2c95b04
MCSeal <mzeal93@gmail.com>
2022-07-24 14:31:47 -0400

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo 'Hello World eat !!me';
+
+$name = 'John';
+$age = '30';
+
+
+$list = [1,2];
+
+var_dump($name);
+
+echo(str_replace('ohn', 'ason', $name));
+
+
+?>
+
+<!-- $con = new mysqli('mysql_db', 'root', 'root', 'mysql');
+if($con)
+{
+    echo 'Connected to MySQL';
+}
+else
+{
+    echo 'Not Connected to MySQL';
+} -->
